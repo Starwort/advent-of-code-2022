53c4b1a640f30a8437c669951e83ff06ec561e76
starwort <tcphone93@gmail.com>
2022-11-29 04:50:57 +0000

    add aoc_helper templates

---
+++ day_22.py
@@ -0,0 +1,33 @@
+import aoc_helper
+from aoc_helper import (
+    decode_text,
+    extract_ints,
+    frange,
+    irange,
+    iter,
+    list,
+    map,
+    range,
+    tail_call,
+)
+
+raw = aoc_helper.fetch(22, 2022)
+
+
+def parse_raw():
+    ...
+
+
+data = parse_raw()
+
+
+def part_one():
+    ...
+
+
+def part_two():
+    ...
+
+
+aoc_helper.lazy_submit(day=22, year=2022, solution=part_one)
+aoc_helper.lazy_submit(day=22, year=2022, solution=part_two)
