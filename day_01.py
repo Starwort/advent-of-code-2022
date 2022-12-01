7404e9ba3f14da286675ae4f7ad0d8cbe3568a17
starwort <tcphone93@gmail.com>
2022-12-01 05:04:02 +0000

    461/357. Silly typo on input parsing cost me lb

--- day_01.py
+++ day_01.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 import aoc_helper
 from aoc_helper import (
     Grid,
@@ -17,18 +19,18 @@ raw = aoc_helper.fetch(1, 2022)
 
 
 def parse_raw():
-    ...
+    return list(raw.split("\n\n")).mapped(extract_ints)
 
 
 data = parse_raw()
 
 
 def part_one():
-    ...
+    return data.mapped(lambda l: l.sum()).max()
 
 
 def part_two():
-    ...
+    return data.mapped(lambda l: l.sum()).sorted(reverse=True)[:3].sum()
 
 
 aoc_helper.lazy_submit(day=1, year=2022, solution=part_one)
