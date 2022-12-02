333e2caca79662330d6cea764599b979060404bc
starwort <tcphone93@gmail.com>
2022-12-02 05:07:49 +0000

    405 / 272

--- day_02.py
+++ day_02.py
@@ -15,20 +15,40 @@ from aoc_helper import (
 
 raw = aoc_helper.fetch(2, 2022)
 
+# raw = """A Y
+# B X
+# C Z"""
+
 
 def parse_raw():
-    ...
+    return list(raw.splitlines()).mapped(str.split)
 
 
 data = parse_raw()
 
 
 def part_one():
-    ...
+    score = 0
+    for move, resp in data:
+        score += " XYZ".index(resp)
+        score += 3 * ("ABC".index(move) == "XYZ".index(resp))
+        score += 6 * ("CAB".index(move) == "XYZ".index(resp))
+    return score
 
 
 def part_two():
-    ...
+    score = 0
+    for move, outcome in data:
+        if outcome == "X":
+            resp = "ZXY"["ABC".index(move)]
+        if outcome == "Y":
+            resp = "XYZ"["ABC".index(move)]
+        if outcome == "Z":
+            resp = "YZX"["ABC".index(move)]
+        score += " XYZ".index(resp)
+        score += 3 * ("ABC".index(move) == "XYZ".index(resp))
+        score += 6 * ("CAB".index(move) == "XYZ".index(resp))
+    return score
 
 
 aoc_helper.lazy_submit(day=2, year=2022, solution=part_one)
