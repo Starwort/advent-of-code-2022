6f48c103062a10f98b9cdcb38805f7b3399dc053
starwort <tcphone93@gmail.com>
2022-12-04 05:08:06 +0000

    65/58

--- day_04.py
+++ day_04.py
@@ -17,18 +17,34 @@ raw = aoc_helper.fetch(4, 2022)
 
 
 def parse_raw():
-    ...
+    return (
+        list(raw.splitlines())
+        .mapped(lambda i: i.split(","))
+        .mapped_each(lambda i: list(map(int, i.split("-"))))
+    )
 
 
 data = parse_raw()
 
 
 def part_one():
-    ...
+    count = 0
+    for a, b in data:
+        if a[0] >= b[0] and a[1] <= b[1]:
+            count += 1
+        elif b[0] >= a[0] and b[1] <= a[1]:
+            count += 1
+    return count
 
 
 def part_two():
-    ...
+    count = 0
+    for a, b in data:
+        if b[0] <= a[0] <= b[1]:
+            count += 1
+        elif a[0] <= b[0] <= a[1]:
+            count += 1
+    return count
 
 
 aoc_helper.lazy_submit(day=4, year=2022, solution=part_one)
