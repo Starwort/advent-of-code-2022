49eb53afa195c8c13d2cbc71ca5e56a621a8933f
starwort <tcphone93@gmail.com>
2022-12-05 05:10:34 +0000

    269 / 195

--- day_05.py
+++ day_05.py
@@ -17,18 +17,37 @@ raw = aoc_helper.fetch(5, 2022)
 
 
 def parse_raw():
-    ...
+    crates, moves = raw.split("\n\n")
+    _crates = crates.splitlines()[:-1]
+    n_crates = (len(_crates[0]) + 1) // 4
+    crates = list(list() for _ in range(n_crates))
+    for i in range(n_crates):
+        for line in _crates[::-1]:
+            if line[i * 4 + 1] == " ":
+                break
+            crates[i].append(line[i * 4 + 1])
+    return crates, extract_ints(moves).chunked(3)
 
 
 data = parse_raw()
 
 
 def part_one():
-    ...
+    crates, moves = data
+    crates = crates.deepcopy()
+    for (n, from_, to) in moves:
+        for _ in range(n):
+            crates[to - 1].append(crates[from_ - 1].pop())
+    return "".join(stack[-1] for stack in crates)
 
 
 def part_two():
-    ...
+    crates, moves = data
+    crates = crates.deepcopy()
+    for (n, from_, to) in moves:
+        to_move = [crates[from_ - 1].pop() for _ in range(n)]
+        crates[to - 1].extend(to_move[::-1])
+    return "".join(stack[-1] for stack in crates)
 
 
 aoc_helper.lazy_submit(day=5, year=2022, solution=part_one)
