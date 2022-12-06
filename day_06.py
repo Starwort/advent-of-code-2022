d71ed661090008108246a37d1ef1af5bc74d8fcb
starwort <tcphone93@gmail.com>
2022-12-06 05:03:53 +0000

    669 / 402

--- day_06.py
+++ day_06.py
@@ -17,18 +17,26 @@ raw = aoc_helper.fetch(6, 2022)
 
 
 def parse_raw():
-    ...
+    return list(raw)
 
 
 data = parse_raw()
 
 
 def part_one():
-    ...
+    return (
+        raw.index("".join(data.windowed(4).filtered(lambda i: len(set(i)) == 4).find()))
+        + 4
+    )
 
 
 def part_two():
-    ...
+    return (
+        raw.index(
+            "".join(data.windowed(14).filtered(lambda i: len(set(i)) == 14).find())
+        )
+        + 14
+    )
 
 
 aoc_helper.lazy_submit(day=6, year=2022, solution=part_one)
