1d3c84d20a54ed6ecddd51df9a9e4d91da9800d5
starwort <tcphone93@gmail.com>
2022-12-08 05:13:33 +0000

    734 / 215 Broken solving code meant I got a bad answer. Broken testing code meant I submitted a bad answer.

--- day_08.py
+++ day_08.py
@@ -17,21 +17,72 @@ raw = aoc_helper.fetch(8, 2022)
 
 
 def parse_raw(raw):
-    ...
+    return Grid.from_string(raw)
 
 
 data = parse_raw(raw)
 
 
-def part_one(data):
-    ...
+def part_one(data: Grid[int]):
+    visible = list(list(False for _ in row) for row in data)
+    for y, row in enumerate(data.data):
+        for x, height in enumerate(row):
+            if (
+                not row[:x]
+                or row[:x].max() < height
+                or not row[x + 1 :]
+                or row[x + 1 :].max() < height
+            ):
+                visible[y][x] = True
+    for x in range(len(data.data[0])):
+        for y in range(len(data.data)):
+            for y_ in range(y):
+                if data[y_][x] >= data[y][x]:
+                    break
+            else:
+                visible[y][x] = True
+            for y_ in range(y + 1, len(data.data)):
+                if data[y_][x] >= data[y][x]:
+                    break
+            else:
+                visible[y][x] = True
+    return visible.mapped(sum).sum()
 
 
 aoc_helper.lazy_test(day=8, year=2022, parse=parse_raw, solution=part_one)
 
 
 def part_two(data):
-    ...
+    scores = list(list(1 for _ in row) for row in data)
+    for y, row in enumerate(data.data):
+        for x, height in enumerate(row):
+            count = 0
+            for x_ in range(x - 1, -1, -1):
+                count += 1
+                if data[y][x_] >= data[y][x]:
+                    break
+            scores[y][x] *= count
+            count = 0
+            for x_ in range(x + 1, len(data[0])):
+                count += 1
+                if data[y][x_] >= data[y][x]:
+                    break
+            scores[y][x] *= count
+    for x in range(len(data.data[0])):
+        for y in range(len(data.data)):
+            count = 0
+            for y_ in range(y - 1, -1, -1):
+                count += 1
+                if data[y_][x] >= data[y][x]:
+                    break
+            scores[y][x] *= count
+            count = 0
+            for y_ in range(y + 1, len(data.data)):
+                count += 1
+                if data[y_][x] >= data[y][x]:
+                    break
+            scores[y][x] *= count
+    return scores.mapped(max).max()
 
 
 aoc_helper.lazy_test(day=8, year=2022, parse=parse_raw, solution=part_two)
