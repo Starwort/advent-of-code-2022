e61fdc4dfa1dcd44ebac9d4b57cde62ebce3ef3e
starwort <tcphone93@gmail.com>
2022-12-11 05:31:16 +0000

    Remove all the debug prints

--- day_11.py
+++ day_11.py
@@ -41,31 +41,17 @@ def part_one(data):
     inspected = [0 for _ in data]
     for _ in range(20):
         for i, monkey in enumerate(data):
-            # print(f"Monkey {i}:")
             for old in monkey["items"]:
-                # print(f"  Monkey inspects an item with a worry level of {old}.")
                 inspected[i] += 1
                 new = 0
                 _locals = {"old": old}
                 exec(monkey["op"], {}, _locals)
                 new = _locals["new"]
-                # print(f"  Worry level is {monkey['op']} to {new}.")
                 new = new // 3
-                # print(f"  Monkey gets bored with item. Worry level is {new}.")
                 if new % monkey["test"] == 0:
-                    # print(f"  Current worry level is divisble by {monkey['test']}")
                     data[monkey["true"]]["items"].append(new)
-                    # print(
-                    #     f"Item with worry level {new} is passed to monkey"
-                    #     f" {monkey['true']}"
-                    # )
                 else:
-                    # print(f"  Current worry level is not divisble by {monkey['test']}")
                     data[monkey["false"]]["items"].append(new)
-                    # print(
-                    #     f"Item with worry level {new} is passed to monkey"
-                    #     f" {monkey['false']}"
-                    # )
             monkey["items"].clear()
     return math.prod(sorted(inspected)[-2:])
 
@@ -79,33 +65,19 @@ def part_two(data):
     loop_factor = math.prod(monkey["test"] for monkey in data)
     for _ in range(10000):
         for i, monkey in enumerate(data):
-            # print(f"Monkey {i}:")
             for old in monkey["items"]:
-                # print(f"  Monkey inspects an item with a worry level of {old}.")
                 inspected[i] += 1
                 new = 0
                 _locals = {"old": old}
                 exec(monkey["op"], {}, _locals)
                 new = _locals["new"]
-                # print(f"  Worry level is {monkey['op']} to {new}.")
                 new = (
                     new
                 ) % loop_factor  # shrink the number without affecting the tests
-                # print(f"  Monkey gets bored with item. Worry level is {new}.")
                 if new % monkey["test"] == 0:
-                    # print(f"  Current worry level is divisble by {monkey['test']}")
                     data[monkey["true"]]["items"].append(new)
-                    # print(
-                    #     f"Item with worry level {new} is passed to monkey"
-                    #     f" {monkey['true']}"
-                    # )
                 else:
-                    # print(f"  Current worry level is not divisble by {monkey['test']}")
                     data[monkey["false"]]["items"].append(new)
-                    # print(
-                    #     f"Item with worry level {new} is passed to monkey"
-                    #     f" {monkey['false']}"
-                    # )
             monkey["items"].clear()
     return math.prod(sorted(inspected)[-2:])
 
