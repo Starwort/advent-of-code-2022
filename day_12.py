6ec9f5de9c3ba04dfcccd18a6ec7a890097f370b
starwort <tcphone93@gmail.com>
2022-12-12 06:27:38 +0000

    Rewrite to use pathfind instead of A*

--- day_12.py
+++ day_12.py
@@ -40,53 +40,10 @@ def parse_raw(raw):
 data = parse_raw(raw)
 
 
-def dist(a, b):
-    return abs(a[0] - b[0]) + abs(a[1] - b[1])
-
-
-def log(val):
-    # print(val)
-    return val
-
-
 def part_one(data):
     grid, pos, target = data
-    visited = set()
-    options = PrioQueue([(0, dist(pos, target), pos, [pos])])
-    for weight, _, (x, y), prevs in options:
-        # print(weight, _, x, y)
-        if (x, y) in visited:
-            continue
-        visited.add((x, y))
-        if (x, y) == target:
-            # print(prevs)
-            return weight
-        node_value = grid[y][x]
-        if x > 0 and (log(grid[y][x - 1] - node_value)) <= 1:
-            options.push(
-                (weight + 1, dist((x - 1, y), target), (x - 1, y), prevs + [(x - 1, y)])
-            )
-        if x < len(grid.data[0]) - 1 and (log(grid[y][x + 1] - node_value)) <= 1:
-            options.push(
-                (weight + 1, dist((x + 1, y), target), (x + 1, y), prevs + [(x + 1, y)])
-            )
-        if y > 0 and (log(grid[y - 1][x] - node_value)) <= 1:
-            options.push(
-                (weight + 1, dist((x, y - 1), target), (x, y - 1), prevs + [(x, y - 1)])
-            )
-        if y < len(grid.data) - 1 and (log(grid[y + 1][x] - node_value)) <= 1:
-            options.push(
-                (weight + 1, dist((x, y + 1), target), (x, y + 1), prevs + [(x, y + 1)])
-            )
-        # print(options._data)
+    return grid.pathfind(pos, target, lambda i, j: j - i <= 1, lambda i, j: 1)
 
-
-raw_t = """Sabqponm
-abcryxxl
-accszExk
-acctuvwj
-abdefghi"""
-data_t = parse_raw(raw_t)
 
 aoc_helper.lazy_test(day=12, year=2022, parse=parse_raw, solution=part_one)
 
